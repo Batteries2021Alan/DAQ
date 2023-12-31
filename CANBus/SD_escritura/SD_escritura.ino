d8764a0dff2de29f1a6d762de980e89795e58104
Batteries2021Alan <fsae.unam.alanm@gmail.com>
2023-12-30 22:27:47 -0600

    Prueba 2
    En esta iteracion el codigo ya mete 50 tramas con ayuda de estructuras

--- CANBus/SD_escritura/SD_escritura.ino
+++ CANBus/SD_escritura/SD_escritura.ino
@@ -1,60 +1,67 @@
-//Prueba
+//Prueba 2
+//----------------------------Librerias------------------------------------------//
 #include <SD.h>
-int sensor2=5;
-int sensor3=10;
-float val=0;
-String nombre="datalogP.csv";
-boolean existe=true, titulosPuestos=false;
-String token=",", datos, cor1="[", cor2="]";
-String tramas[50][7];
-File Data;
+#include <SPI.h> //Library for using SPI Communication
+#include <LinkedList.h> //Library for using Linked List commands
+//----------------------------Variables globales------------------------------------------//
+int sensor2=5;//Ya no se usa
+int sensor3=10;// Ya no se usa
+float val=0;//No importa si se pone(Solo es para checar cuantos datos se pueden poner por celda)
+String nombre="datalogP.csv";//Este es el titulo
+boolean existe=false, titulosPuestos=false;//Variables de verificacion para saber si algo ya paso o todavia no
+String token=",", datos, cor1="[", cor2="]",d1="",sensor="Sensor";//Variables para la escritura (el mas usado y mas importante es el token)
+//String tramas[50][7];//estas son el numero de celdas que se quieren usar pero no sirve
+LinkedList<String> DatosDeTramas=LinkedList<String>();
+File Data;//Variable para un archivo
 
+//----------------------------setup------------------------------------------//
 void setup()
 {
   Serial.begin(9600);
-  Serial.print("Iniciando SD ...");
+  Serial.println("Iniciando SD ...");
   if (!SD.begin(4)) {
     Serial.println("No se pudo inicializar");
     return;
   }
   Serial.println("inicializacion exitosa");
 }
-
+//----------------------------loop------------------------------------------//
 void loop()
 {
-  if(SD.exists(nombre)&&existe==true){
+  if(SD.exists(nombre)&&existe==0){
     SD.remove(nombre);
-    Serial.println("Si existe y se borro");
-    existe=false;
+    Serial.print("Si existe y se borro");
+    existe=1;
+    Serial.println(existe);
   }
   Data = SD.open(nombre,FILE_WRITE);//abrimos  el archivo
-  if (Data) { 
-        Serial.print("Escribiendo SD: ");
+  if (Data) {
         if(titulosPuestos==false){
           Data.print("Tiempo,Prueba,");
           for(int i=1;i<=350;i++){
              if(i<350){
-              Data.print("Sensor"+cor1+i+cor2+token);
+              Data.print(sensor+cor1+i+cor2+token);
               val=100000.8851+i;
               datos=val+token+datos+token;
              }
              else{
-              Data.println("Sensor"+cor1+i+cor2);
+              Data.println(sensor+cor1+i+cor2);
              }
              delay(1);
           }
-          Serial.println("Titulo puesto");
-          titulosPuestos=true;
-        }
-        //Data.print(buf);
-        for(int j=0;j<50;j++){
-          for(int k=0;k<7;k++){
-            tramas[j][k]=k+token;
+          for(int j=1;j<=7;j++){
+            d1=(String)j+",";
+            DatosDeTramas.add(d1);
+            Serial.print("Entra 1 "+d1);
           }
+          titulosPuestos=true;
+          Serial.println("Titulos puesto");
         }
+        Serial.print("Escribiendo SD: ");
+        //Data.print(buf);
         Data.print(millis()/1000);
         Data.print(token);
-        Data.print("prueba 2");
+        Data.print("prueba 2.2");
         Data.print(token);/*
         Data.print(analogRead(A0));
         Data.print(",");
@@ -62,15 +69,18 @@ void loop()
         Data.print(",");
         Data.print(sensor3);
         Data.print(",");*/
-        for(int j=0;j<50;j++){
-          for(int k=0;k<7;k++){
-            if(j<49){
-              Data.print(tramas[j][k]);
+        for(int h=0;h<50;h++){
+          for(int j=0;j<DatosDeTramas.size();j++){
+            if(h<49){
+              Data.print(DatosDeTramas.get(j));
             }
             else{
-              Data.println(tramas[j][k]);
+              if(j<6)
+                Data.print(DatosDeTramas.get(j));
+              else
+                Data.println(DatosDeTramas.get(j));
             }
-            delay(10);
+            delay(1);
           }
         }
         //Data.println(datos);
